6ab2bd10387b61935982d3edc8acfa02239e584a
alfredocr <alfredoc.rossi@gmail.com>
2022-12-11 00:38:19 -0300

    creación de las Apps

--- Proyecto_CaC/Proyecto_Final/Proyecto_Final/settings.py
+++ Proyecto_CaC/Proyecto_Final/Proyecto_Final/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'emergencias',
+    'registration',
+    'turnos',
+ 
+  
+
+
 ]
 
 MIDDLEWARE = [
@@ -103,8 +110,10 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+# Ajustamos el lenguaje
+LANGUAGE_CODE = 'es-ar'
 
+# Ajustamos la zona horaria
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
